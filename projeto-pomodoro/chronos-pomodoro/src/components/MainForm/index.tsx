b1b1c859f06306bcd095dcc3b1fe404aa92bcb13
Lu-2112 <eduardadfluiza@gmail.com>
2026-05-18 15:48:31 -0300

    feat: completando mais uma etapa de praticas

--- projeto-pomodoro/chronos-pomodoro/src/components/MainForm/index.tsx
+++ projeto-pomodoro/chronos-pomodoro/src/components/MainForm/index.tsx
@@ -1,28 +1,85 @@
-// src/components/MainForm/index.tsx
 import { PlayCircleIcon } from 'lucide-react';
+import { useRef } from 'react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import type { TaskModel } from '../../models/TaskModel';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
+  
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (taskNameInput.current === null) return;
+    const taskName = taskNameInput.current.value.trim();
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const nextDuration = state.config[nextCycleType];
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(), 
+      completeDate: null,
+      interruptDate: null,
+      duration: nextDuration, 
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining, 
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining), 
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+
+    taskNameInput.current.value = '';
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
           id='meuInput'
           type='text'
           placeholder='Digite algo'
+          ref={taskNameInput}
+    
+          disabled={!!state.activeTask}
         />
       </div>
 
       <div className='formRow'>
-        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Próximo intervalo é de 25min</p>
       </div>
 
-      <div className='formRow'>
-        <Cycles />
-      </div>
+      {state.currentCycle > 0 && (
+        <div className='formRow'>
+          <Cycles />
+        </div>
+      )}
 
       <div className='formRow'>
         <DefaultButton icon={<PlayCircleIcon />} />
--- projeto-pomodoro/chronos-pomodoro/src/pages/Home/index.tsx
+++ projeto-pomodoro/chronos-pomodoro/src/pages/Home/index.tsx
@@ -1,13 +1,12 @@
-// src/pages/Home/index.tsx
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
 import { MainForm } from '../../components/MainForm';
 import { MainTemplate } from '../../templates/MainTemplate';
 
 export function Home() {
+  // Código limpo, sem nenhuma prop intermediária!
   return (
     <MainTemplate>
-      {/* O cronômetro e o formulário entram como "children" do template */}
       <Container>
         <CountDown />
       </Container>
